135428bfd551608c847018f0a182709742824d13
rcbrauna <eo_plank@outlook.com>
2023-05-07 18:22:10 -0300

    feature: addItem.cy.js

---
+++ cypress/e2e/addItem.cy.js
@@ -0,0 +1,5 @@
+describe("Add Itens", () => {
+  it("Add Todo Itens", () => {
+    cy.addItem("Teste");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("addItem", (text) => {
+  cy.get(".new-todo")
+    .should("be.visible")
+    .type(`${text}{enter}`);
+});
